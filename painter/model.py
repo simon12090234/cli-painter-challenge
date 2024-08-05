5f4a962e229710ccf9d5b7c5f8b0f847925cf08c
simon12090234 <corrasimon82@gmail.com>
2024-08-05 13:52:51 -0500

    modelo terminado

--- painter/model.py
+++ painter/model.py
@@ -1,5 +1,5 @@
-# TODO: Add code here
 import matplotlib.pyplot as plt
+import pickle
 
 
 class Point:
@@ -30,22 +30,86 @@ class Circle:
 
 
 class Triangle:
-            def __init__(self, point_1: Point, point_2: Point, point_3: Point):
-                self.point_1: Point = point_1
-                self.point_2: Point = point_2
-                self.point_3: Point = point_3
+    def __init__(self, point_1: Point, point_2: Point, point_3: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+        self.point_3: Point = point_3
 
-            def area(self):
-                return abs((self.point_1.x * (self.point_2.y - self.point_3.y) +
-                            self.point_2.x * (self.point_3.y - self.point_1.y) +
-                            self.point_3.x * (self.point_1.y - self.point_2.y)) / 2)
+    def area(self) -> float:
+        return abs((self.point_1.x * (self.point_2.y - self.point_3.y) +
+                    self.point_2.x * (self.point_3.y - self.point_1.y) +
+                    self.point_3.x * (self.point_1.y - self.point_2.y)) / 2)
 
-            def draw(self):
-                x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
-                y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
-                plt.fill(x, y, color='b')
-                plt.axis("scaled")
-                plt.show()
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+                x1, y1 = self.point_1
+                x2, y2 = self.point_2
+                x3, y3 = self.point_3
+                return f"Triangle with vertices at ({x1, y1}), ({x2, y2}) and ({x3, y3})"
+
+
+class Rectangle:
+    def __init__(self, point_1: Point, point_2: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+
+    def area(self) -> float:
+        return abs((self.point_2.x - self.point_1.x) * (self.point_2.y - self.point_1.y))
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_2.x, self.point_1.x, self.point_1.x]
+        y = [self.point_1.y, self.point_1.y, self.point_2.y, self.point_2.y, self.point_1.y]
+        plt.fill(x, y, color='g')
+        plt.axis("scaled")
+        plt.show()
+    def __str__(self):
+        x1, y1 = self.point_1
+        x2, y2 = self.point_2
+        return f"Rectangle with vertices at ({x1, y1}) and ({x2, y2})"
+
+
+class Painter:
+
+    FILE = ".painter"
+
+    def _init_(self) -> None:
+        self.shapes: list = []
+        self._load()
+
+    def _load(self) -> None:
+        try:
+            with open(Painter.FILE, "rb") as f:
+                self.shapes = pickle.load(f)
+        except (EOFError, FileNotFoundError):
+            self.shapes = []
+
+    def _save(self) -> None:
+        with open(Painter.FILE, "wb") as f:
+            pickle.dump(self.shapes, f)
+
+    def add_shape(self, shape) -> None:
+        self.shapes.append(shape)
+        self._save()
+
+    def total_area(self) -> float:
+        return sum(shape.area() for shape in self.shapes)
+
+    def clear(self) -> None:
+        self.shapes = []
+        self._save()
+
+
+
+
+
+
+
 
 
 
